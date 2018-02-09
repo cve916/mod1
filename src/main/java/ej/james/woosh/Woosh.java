a0819225dc94392c44b5fdd953c8de073faa3a63
dohmen <dohmen@csus.edu>
2018-02-09 10:55:11 -0800

    adding Woosh

--- src/main/java/ej/james/woosh/Woosh.java
+++ src/main/java/ej/james/woosh/Woosh.java
@@ -3,6 +3,7 @@ package ej.james.woosh;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
@@ -13,16 +14,30 @@ public class Woosh {
     public static final String NAME = "Whoosh Mod";
     public static final String VERSION = "1.0";
 
+    @SidedProxy(clientSide = "", serverSide = "")
+    public static CommonProxy proxy;
+
     private static Logger logger;
 
     @EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        proxy.preInit(event);
         logger = event.getModLog();
     }
 
     @EventHandler
-    public void init(FMLInitializationEvent event) {
+    public void init(FMLInitializationEvent event)
+    {
+        proxy.postInit(event);
         // some example code
         logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+
+    @EventHandler
+    public void postInit(FMLInitializationEvent event)
+    {
+        proxy.postInit(event);
+
     }
 }
