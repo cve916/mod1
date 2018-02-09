dead42405c54dbcfa57f2a6019431750bd501f9f
dohmen <dohmen@csus.edu>
2018-02-09 11:05:38 -0800

    thing

--- src/main/java/ej/james/woosh/Woosh.java
+++ src/main/java/ej/james/woosh/Woosh.java
@@ -1,10 +1,12 @@
 package ej.james.woosh;
 
+import ej.james.woosh.proxy.CommonProxy;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
@@ -14,8 +16,9 @@ public class Woosh {
     public static final String NAME = "Whoosh Mod";
     public static final String VERSION = "1.0";
 
-    @SidedProxy(clientSide = "", serverSide = "")
+    @SidedProxy(clientSide = "ej.james.woosh.proxy.ClientProxy", serverSide = "ej.james.woosh.proxy.CommonProxy")
     public static CommonProxy proxy;
+
 
     private static Logger logger;
 
@@ -29,13 +32,13 @@ public class Woosh {
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        proxy.postInit(event);
+        proxy.init(event);
         // some example code
         logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
 
     @EventHandler
-    public void postInit(FMLInitializationEvent event)
+    public void postInit(FMLPostInitializationEvent event)
     {
         proxy.postInit(event);
 
